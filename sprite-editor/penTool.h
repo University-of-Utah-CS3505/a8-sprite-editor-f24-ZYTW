c9534081fc453307f6d272837226a0cf69476d20
iiliki <iiliki@iilikis-MacBook-Air.local>
2024-11-14 02:23:32 -0700

    Added documentation

--- sprite-editor/penTool.h
+++ sprite-editor/penTool.h
@@ -15,7 +15,17 @@
 
 class Pen : public Tool {
 public:
+    /**
+     * @brief Initializaes a Pen object with a specified color to paint pixels on the grid.
+     * @param Selected Color of the pen.
+     */
     Pen(QColor color) { this->color = color; }
+    /**
+     * @brief Initializaes a Pen object with a specified color to paint pixels on the grid
+     * @param x The x-coordinate of the pixel to be colored.
+     * @param y The y-coordinate of the pixel to be colored.
+     * @param a vector of the Pixel obects on the pixel grid.
+     */
     void useTool(int x, int y, std::vector<std::vector<Pixel>>& pixels) override;};
 
 #endif // PENTOOL_H
