f6f7ea9ab65db7954fb9d9f72b20c4df19d19abc
iiliki <iiliki@iilikis-MacBook-Air.local>
2024-11-14 02:34:11 -0700

    Added Documentation

--- sprite-editor/colorPalette.h
+++ sprite-editor/colorPalette.h
@@ -17,15 +17,33 @@ class ColorPalette : public QWidget
 {
     Q_OBJECT
 public:
+    /**
+     * @class ColorPalette
+     * @brief A UI widget for selecting and managing colors in the sprite editor.
+     */
     ColorPalette(QWidget *parent = nullptr);
-
+    /**
+     * @brief Gets the currently selected color in the color pallete
+     * @return QColor The currently selected color.
+     */
     QColor getCurrentColor() const { return currentColor; }
 
 signals:
+    /**
+     * @brief Signal emitted when a new color is selceted
+     * @param Connected slots when a color has been chosen in the palette
+     */
     void colorSelected(QColor color);
 
 public slots:
+    /**
+     * @brief Opens the color dialog for color selection
+     */
     void openColorDialog();
+    /**
+     * @brief Sets the currently selected color
+     * @param Color New color of set as a current color
+     */
     void setColor(QColor color);
 
 private:
